19819e4a78390355dfce948765110948bf8124fa
merknu <Merodningen.Design@gmail.com>
2023-11-13 09:15:38 +0100

    Update file_utils.py
    Error Handling in load_config - added error handling for both FileNotFoundError and json.JSONDecodeError, making your function more robust against common file and format issues.
    Logging Setup -  set up basic logging configuration in the main execution block.
    Modular Function Design - functions are well-organized, each with a clear purpose, making the code easier to understand and modify.
    Path Handling - Using os.path.join for constructing file paths ensures cross-platform compatibility.

--- file_handler/file_utils.py
+++ file_handler/file_utils.py
@@ -1,108 +1,60 @@
 # Path: file_handler\file_utils.py
 # This file is used to handle the files
-from hashlib import sha256
 import os
-import shutil
 import logging
-from PIL import Image
-from tinytag import TinyTag
-from collections import defaultdict
-import textract
-import subprocess
 import json
+from collections import defaultdict
+from metadata_handlers import get_image_size, get_audio_duration, get_document_word_count, get_video_duration
+from file_operations import move_file, preserve_timestamps, calculate_file_hash
 
-
-# this function is used to load the configuration file
+# Load configuration from a JSON file
 def load_config(config_file):
-    with open(config_file) as f:
-        return json.load(f)
+    try:
+        with open(config_file) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        logging.error(f"Configuration file {config_file} not found.")
+        return None
+    except json.JSONDecodeError:
+        logging.error(f"Configuration file {config_file} is not a valid JSON.")
+        return None
 
-
-# This function is used to move the files from the source folder to the destination folder
-def calculate_file_hash(file_path):
-    with open(file_path, "rb") as file:
-        file_hash = sha256(file.read()).hexdigest()
-    return file_hash
-
-
-# This function is used to handle the duplicate files
+# Handle duplicate files
 def handle_duplicate(src, dest, default_action):
     action = input(f"Duplicate file detected: {src}\n"
                    f"Destination: {dest}\n"
                    f"Options: (k)eep, (o)verwrite, (r)ename [default: {default_action}]: ")
     return action.lower() if action else default_action
 
-
-# This function is used to preserve the timestamps of the files in the source folder to the destination folder
-def preserve_timestamps(src, dest):
-    stat_info = os.stat(src)
-    os.utime(dest, (stat_info.st_atime, stat_info.st_mtime))
-
-
-# This function is used to move the files from the source folder to the destination folder
-def move_file(src, dest):
-    shutil.move(src, dest)
-    preserve_timestamps(src, dest)
-
-
-# This function is used to get the size of an image file
-def get_image_size(file_path):
-    with Image.open(file_path) as img:
-        return img.size
-
-
-# This function is used to get the duration of an audio file
-def get_audio_duration(file_path):
-    tag = TinyTag.get(file_path)
-    return tag.duration
-
-
-# This function is used to get the word count of a document
-def get_document_word_count(file_path):
-    text = textract.process(file_path).decode('utf-8')
-    return len(text.split())
-
-
-# This function is used to get the duration of a video file using ffprobe (ffmpeg) command line tool
-def get_video_duration(file_path):
-    supported_formats = [".mp4", ".avi", ".mov", ".mkv", ".wmv"]
-    _, ext = os.path.splitext(file_path)
-
-    if ext.lower() not in supported_formats:
-        raise ValueError(f"Unsupported video format: {ext}")
-
-    cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {file_path}"
-    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode("utf-8").strip()
-
-    return int(float(output))
-
-
-# This function is used to organize the files by metadata
+# Organize files by metadata
 def organize_by_metadata(file_path, ext, app_config):
     file_categories = app_config.get("file_categories", {})
     document_subfolders = app_config.get("subfolders", {})
 
     for category, extensions in file_categories.items():
         if ext in extensions:
-            if category == "Images":
-                width, height = get_image_size(file_path)
-                return f"Images/{width}x{height}"
-            elif category == "Audio":
-                duration = int(get_audio_duration(file_path))
-                return f"Audio/{duration}s"
-            elif category == "Documents":
-                subfolder = document_subfolders.get(ext.lower(), "Other Documents")
-                return f"Documents/{subfolder}"
-            elif category == "Video":
-                duration = int(get_video_duration(file_path))
-                return f"Video/{duration}s"
-            else:
-                return category
+            return handle_category(file_path, category, ext, document_subfolders)
 
     return "Others"
 
+# Handle each file category
+def handle_category(file_path, category, ext, document_subfolders):
+    if category == "Images":
+        width, height = get_image_size(file_path)
+        return f"Images/{width}x{height}"
+    elif category == "Audio":
+        duration = int(get_audio_duration(file_path))
+        return f"Audio/{duration}s"
+    elif category == "Documents":
+        subfolder = document_subfolders.get(ext.lower(), "Other Documents")
+        return f"Documents/{subfolder}"
+    elif category == "Video":
+        duration = int(get_video_duration(file_path))
+        return f"Video/{duration}s"
+    else:
+        return category
 
-# This function is used to organize the files
+# Organize files in a given folder
 def organize_files(folder, app_config, recursive=None, preview_mode=False, callback=None):
     summary = defaultdict(int)
     for file in os.listdir(folder):
@@ -119,7 +71,6 @@ def organize_files(folder, app_config, recursive=None, preview_mode=False, callb
         ext = ext.lower()
 
         subfolder = organize_by_metadata(src, ext, app_config)
-
         target_folder = os.path.join(folder, subfolder)
         os.makedirs(target_folder, exist_ok=True)
 
@@ -139,7 +90,7 @@ def organize_files(folder, app_config, recursive=None, preview_mode=False, callb
                 move_file(src, dest)
                 summary['moved'] += 1
                 logging.info(f"Moved {src} to {dest}")
-                if callback:  # callback is used to update the progress bar in the GUI
+                if callback:
                     callback()
             except FileNotFoundError as e:
                 summary['skipped'] += 1
@@ -149,4 +100,14 @@ def organize_files(folder, app_config, recursive=None, preview_mode=False, callb
             logging.info(f"Preview: {src} would be moved to {dest}")
             if callback:
                 callback()
-        return summary
+
+    return summary
+
+# Main execution
+if __name__ == "__main__":
+    # Setup logging (if not already set up)
+    logging.basicConfig(level=logging.INFO)
+
+    app_config = load_config("config.json")
+    if app_config:
+        organize_files(os.path.join("path", "to", "folder"), app_config, recursive=True, preview_mode=False)
